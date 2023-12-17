062a2ae87048b6ea692b6ffdbfa0b08e8261cbdf
probodis <probodis@gmail.com>
2023-12-17 23:44:39 +0400

    Added the display of file id after uploading; Added a check before uploading that the file is selected

--- src/ui_service/main.py
+++ src/ui_service/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, UploadFile, File
+from fastapi import FastAPI, Request, UploadFile, File, Depends
 from fastapi.templating import Jinja2Templates
 from secrets import token_hex
 from src.config import UPLOADED_FILES_PATH
@@ -15,14 +15,19 @@ def main_page(request: Request):
 
 
 @app.post("/upload")
-async def upload(file: UploadFile = File(...)):
+async def upload_and_queue(file: UploadFile = File(...)):
     file_ext = file.filename.split(".").pop()
     file_name = token_hex(10)
     file_path = f"{UPLOADED_FILES_PATH}\{file_name}.{file_ext}"
     with open(file_path, "wb") as f:
         content = await file.read()
         f.write(content)
-    return {"success": True, "file_path": file_path, "message": "File uploaded successfully"}
+    return {"success": True, "file_path": file_path, "file_id": file_name, "message": "File uploaded successfully"}
+
+
+@app.post("/upload-page")
+def upload_page(request: Request, file: UploadFile = File(...), result=Depends(upload_and_queue)):
+    return templates.TemplateResponse("upload_status.html", {"request": request, "uploading_result": result})
 
 
 if __name__ == '__main__':
