f8da1f319c5c5521e40f51a298e6a5d732dcd84e
probodis <probodis@gmail.com>
2023-12-19 17:05:21 +0400

    added Status column in db as Promise type

--- src/models.py
+++ src/models.py
@@ -2,6 +2,13 @@ from datetime import datetime
 from typing import Optional
 from sqlalchemy import text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import enum
+
+
+class Promise(enum.Enum):
+    fulfilled = "fulfilled"
+    rejected = "rejected"
+    pending = "pending"
 
 
 class Base(DeclarativeBase):
@@ -15,4 +22,5 @@ class UserFileOrm(Base):
     file_name: Mapped[str] = mapped_column(nullable=False)
     file_id: Mapped[str] = mapped_column(nullable=False)
     md5: Mapped[Optional[str]]
+    status: Mapped[Promise] = mapped_column(nullable=False, default=Promise.pending)
     created: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
