d25930a2263213f43edd8ba46dc9d1e63371dc86
probodis <probodis@gmail.com>
2023-12-22 21:29:44 +0400

    DLM by Redis; logging the hashing result

--- src/config.py
+++ src/config.py
@@ -9,9 +9,15 @@ ROOT_DIR = os.path.split(SRC_FOLDER_PATH)[0]
 UPLOADED_FILES_PATH = os.environ.get("UPLOADED_FILES_PATH")
 UPLOADED_FILES_PATH = os.path.join(ROOT_DIR, UPLOADED_FILES_PATH)
 
+LOG_PATH = os.environ.get("LOG_PATH")
+LOG_PATH = os.path.join(ROOT_DIR, LOG_PATH)
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_NAME = os.environ.get("DB_NAME")
+
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT")
 
--- src/md5_service/worker.py
+++ src/md5_service/worker.py
@@ -1,13 +1,13 @@
 import hashlib
-import time
 
+from redlock import Redlock
 from celery import Celery
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, LOG_PATH, REDIS_HOST, REDIS_PORT
 
 result_backend = f'db+postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+celery = Celery('worker', broker=f'redis://{REDIS_HOST}:{REDIS_PORT}', result_backend=result_backend)
 
-celery = Celery('worker', broker='redis://localhost:6379', result_backend=result_backend)
+dlm = Redlock([{"host": REDIS_HOST, "port": REDIS_PORT, "db": 0}, ], retry_count=10, retry_delay=1)
 
 
 @celery.task
@@ -17,4 +17,16 @@ def get_md5_hash(file_path):
         md5 = hashlib.md5()
         md5.update(content)
 
+        write_log(md5.hexdigest())
+
         return md5.hexdigest()
+
+
+def write_log(message: str):
+    lock = dlm.lock("log_file", 5)
+
+    with open(f"{LOG_PATH}", 'a+') as file:
+        file.write(message + "\n")
+
+    if lock:
+        dlm.unlock(lock)
